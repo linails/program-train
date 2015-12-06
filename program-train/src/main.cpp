c6c230bce865149042ccc7b71bfd6161fe5d6dc4
minphone <linails@foxmail.com>
2015-12-06 11:24:56 +0800

    add sqlite.c sqlite.h

--- program-train/src/main.cpp
+++ program-train/src/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include "object_c.h"
+#include "sqlite3.h"
 
 using std::cout;
 using std::endl;
 
 int main(void)
 {
+    sqlite3 *db = NULL;
+    sqlite3_open("first.db",&db);
     cout << "hello world ..." <<endl;
+    sqlite3_close(db);
     return 0;
 }
 
