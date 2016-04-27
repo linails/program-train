758d88b2f72acd69caa67496598bc198adb26981
minphone <linails@foxmail.com>
2016-04-27 23:06:35 +0800

    only use cmake

---
+++ program-train/src/srcs/sort/insertion_sort.c
@@ -0,0 +1,13 @@
+/*
+ * Progarm Name: insertion_sort.c
+ * Created Time: 2016-04-27 23:00:51
+ * Last modified: 2016-04-27 23:02:03
+ * @author: minphone.linails linails@foxmail.com 
+ * @version 0.0.1
+ */
+
+#include "insertion_sort.h"
+#include <stdio.h>
+
+
+
---
+++ program-train/src/srcs/sort/insertion_sort.h
@@ -0,0 +1,23 @@
+/*
+ * Progarm Name: insertion_sort.h
+ * Created Time: 2016-04-27 23:00:58
+ * Last modified: 2016-04-27 23:01:49
+ * @author: minphone.linails linails@foxmail.com 
+ * @version 0.0.1
+ */
+
+#ifndef _INSERTION_SORT_H_
+#define _INSERTION_SORT_H_
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef  __cplusplus
+}
+#endif
+#endif //_INSERTION_SORT_H_
+
