cf8f5366e96c2e240558a76a577d52702eebc578
minphone <linalis@foxmail.com>
2016-06-21 15:28:56 +0800

    update c++11 regex test

--- program-train/src/srcs/c_src/regex_cpp11.cpp
+++ program-train/src/srcs/c_src/regex_cpp11.cpp
@@ -1,7 +1,7 @@
 /*
  * Progarm Name: regex_cpp11.cpp
  * Created Time: 2016-06-16 11:24:06
- * Last modified: 2016-06-20 22:45:30
+ * Last modified: 2016-06-21 14:42:39
  * @author: minphone.linails linails@foxmail.com 
  * @version 0.0.1
  */
@@ -92,9 +92,10 @@ void regex_cpp11::regex_t4(void)
             cout << "*i : " << *i << endl;
         }
     }
+
+    string str("亶1	`1`亶1`2`dǎn<br>(1)<br>(形声。本义:谷多)<br>(2)<br>同本义 [full of grains]<br>亶,多谷也。――《说文》<br>择三有事,亶侯多藏。――《诗·小雅·十月之交》<br>(3)<br>厚道;忠实 [kind and sincere]<br>亶,信也;又,诚也。――《尔雅》<br>不实于亶。――《诗·大雅·板》<br>(4)<br>又如:亶厚(忠厚,淳厚);亶诚(真诚)<br>(5)<br>平坦;广大 [smooth]。如:亶亶(平坦。通“坦坦”)<br>(6)<br>姓");
     {
         cout << "---------------------------------------------------------" << endl;
-        string str("亶1	`1`亶1`2`dǎn<br>(1)<br>(形声。本义:谷多)<br>(2)<br>同本义 [full of grains]<br>亶,多谷也。――《说文》<br>择三有事,亶侯多藏。――《诗·小雅·十月之交》<br>(3)<br>厚道;忠实 [kind and sincere]<br>亶,信也;又,诚也。――《尔雅》<br>不实于亶。――《诗·大雅·板》<br>(4)<br>又如:亶厚(忠厚,淳厚);亶诚(真诚)<br>(5)<br>平坦;广大 [smooth]。如:亶亶(平坦。通“坦坦”)<br>(6)<br>姓");
 
         const char pastr[] = "`\\d`[^<]{0,4}";
         regex pattern(pastr);
@@ -113,7 +114,6 @@ void regex_cpp11::regex_t4(void)
     }
     {
         cout << "---------------------------------------------------------" << endl;
-        string str("亶1	`1`亶1`2`dǎn<br>(1)<br>(形声。本义:谷多)<br>(2)<br>同本义 [full of grains]<br>亶,多谷也。――《说文》<br>择三有事,亶侯多藏。――《诗·小雅·十月之交》<br>(3)<br>厚道;忠实 [kind and sincere]<br>亶,信也;又,诚也。――《尔雅》<br>不实于亶。――《诗·大雅·板》<br>(4)<br>又如:亶厚(忠厚,淳厚);亶诚(真诚)<br>(5)<br>平坦;广大 [smooth]。如:亶亶(平坦。通“坦坦”)<br>(6)<br>姓");
 
         regex pattern("^[^`]{1,5}");
         sregex_token_iterator end;
@@ -131,9 +131,8 @@ void regex_cpp11::regex_t4(void)
     }
     {
         cout << "---------------------------------------------------------" << endl;
-        string str("亶1	`1`亶1`2`dǎn<br>(1)<br>(形声。本义:谷多)<br>(2)<br>同本义 [full of grains]<br>亶,多谷也。――《说文》<br>择三有事,亶侯多藏。――《诗·小雅·十月之交》<br>(3)<br>厚道;忠实 [kind and sincere]<br>亶,信也;又,诚也。――《尔雅》<br>不实于亶。――《诗·大雅·板》<br>(4)<br>又如:亶厚(忠厚,淳厚);亶诚(真诚)<br>(5)<br>平坦;广大 [smooth]。如:亶亶(平坦。通“坦坦”)<br>(6)<br>姓");
 
-        regex pattern("\(\\d\).+(?=>\(\\d)");
+        regex pattern("\\(\\d\\)[^\\(]+");
         sregex_token_iterator end;
 
         vector<string> units;
@@ -145,6 +144,63 @@ void regex_cpp11::regex_t4(void)
         for(auto iter = units.begin();
                  iter!= units.end(); iter++){
             cout << "iter : " << *iter << endl;
+
+            vector<string> subunits;
+            sregex_token_iterator subend;
+            regex subpattern("<br>[^<]+");
+            for(sregex_token_iterator i(iter->begin(), iter->end(), subpattern); i!=subend; ++i){
+                subunits.push_back(*i);
+            }
+            for(auto iiter = subunits.begin();
+                     iiter!= subunits.end(); iiter++){
+                cout << "iiter : " << *iiter << endl;
+            }
+        }
+    }
+    {
+        cout << "---------------------------------------------------------" << endl;
+
+        regex pattern("br(?=>\\(\\d\\))");
+        sregex_token_iterator end;
+
+        vector<string> units;
+
+        for(sregex_token_iterator i(str.begin(), str.end(), pattern); i!=end; ++i){
+            cout << "*i : " << *i << endl;
+            units.push_back(*i);
+        }
+        for(auto iter = units.begin();
+                 iter!= units.end(); iter++){
+            cout << "iter : " << *iter << endl;
+        }
+    }
+    {
+        cout << "---------------------------------------------------------" << endl;
+
+        regex pattern("(\\(\\d\\)){1}.+?(?=>\\(\\d\\))|\\(\\d\\).*");
+        sregex_token_iterator end;
+
+        vector<string> units;
+
+        for(sregex_token_iterator i(str.begin(), str.end(), pattern); i!=end; ++i){
+            cout << "*i : " << *i << endl;
+            units.push_back(*i);
+        }
+        for(auto iter = units.begin();
+                 iter!= units.end(); iter++){
+            cout << "iter : " << *iter << endl;
+
+            vector<string> subunits;
+            sregex_token_iterator subend;
+            regex subpattern("<br>[^<]+");
+            for(sregex_token_iterator i(iter->begin(), iter->end(), subpattern); i!=subend; ++i){
+                subunits.push_back(*i);
+            }
+            for(auto iiter = subunits.begin();
+                     iiter!= subunits.end(); iiter++){
+                cout << "iiter : " << *iiter << endl;
+            }
+
         }
     }
 }
