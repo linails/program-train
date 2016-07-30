8bc1f1b33ec9da82f2f6619ae002b64f42c78aed
minphone <linalis@foxmail.com>
2016-07-30 22:11:48 +0800

    call_once & sync_queue

--- program-train/src/srcs/c_src/condition_variable.cpp
+++ program-train/src/srcs/c_src/condition_variable.cpp
@@ -1,7 +1,7 @@
 /*
  * Progarm Name: condition_variable.cpp
  * Created Time: 2016-07-30 09:40:53
- * Last modified: 2016-07-30 10:05:04
+ * Last modified: 2016-07-30 22:07:29
  */
 
 #include "condition_variable.h"
@@ -22,8 +22,13 @@ void do_print_id(int id)
 {
 #if 1
     unique_lock<mutex> locker(mtx);
+    int i = 0;
 
     while(false == ready){
+        cout << "while " << i++ << endl;
+        /* 进入wait 状态后，线程就相当于是被挂起等待，
+         * 不消耗cpu资源
+         * 相比于不断的进行轮询，对cpu的利用效率提升很多*/
         cv.wait(locker);
     }
 #else
@@ -48,21 +53,48 @@ void go(void)
 #endif
 }
 
+/* std::once_flag */
+once_flag flag;
+
+void do_once(void)
+{
+    call_once(flag, []{cout << "called once" << endl;});
+}
+
+void do_multi(void)
+{
+    cout << "multi..." << endl;
+}
+
 void cv_main(void)
 {
-    thread threads[10];
-    for(int i=0; i<10; i++){
-        threads[i] = thread(do_print_id, i);
+    {
+        thread threads[10];
+        for(int i=0; i<10; i++){
+            threads[i] = thread(do_print_id, i);
+        }
+
+        cout << "10 threads ready to race..." << endl;
+
+        /* C++11 sleep */
+        this_thread::sleep_for(chrono::seconds(2));
+        //sleep(2);
+
+        go();
+
+        for(auto &th : threads) th.join();
     }
-
-    cout << "10 threads ready to race..." << endl;
-
-    /* C++11 sleep */
-    this_thread::sleep_for(chrono::seconds(2));
-    //sleep(2);
-
-    go();
-
-    for(auto &th : threads) th.join();
+    cout << "----------------------------" << endl;
+    {
+        thread ths[5];
+        for(auto &th: ths)  th = thread(do_multi);
+        for(auto &th: ths)  th.join();
+    }
+    cout << "----------------------------" << endl;
+    {
+        thread ths[10];
+        for(auto &th: ths)  th = thread(do_once);
+        for(auto &th: ths)  th.join();
+    }
 }
 
