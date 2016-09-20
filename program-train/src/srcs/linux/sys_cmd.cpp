77e2cfe419ef11e7a5738440648c77fb73fb7c6c
minphone <linalis@foxmail.com>
2016-09-20 17:26:46 +0800

    update system cmd called in c++

--- program-train/src/srcs/linux/sys_cmd.cpp
+++ program-train/src/srcs/linux/sys_cmd.cpp
@@ -1,7 +1,7 @@
 /*
  * Progarm Name: sys_cmd.cpp
  * Created Time: 2016-09-20 10:59:19
- * Last modified: 2016-09-20 11:08:23
+ * Last modified: 2016-09-20 17:25:26
  */
 
 #include "sys_cmd.hpp"
@@ -16,18 +16,53 @@ using namespace std;
 
 void sys_cmd_called(void)
 {
-    char line[1024];
     FILE *fp = NULL;
 
-    string cmd = "ls -l ~/share/temp";
+    auto do_shell = [&](const char *str,
+                 const char *in = NULL,
+                 const char *judge = NULL){
+        char line[1024];
+        char wr[2] = "r";
+        if(NULL != in) wr[0] = 'w';
+        if(NULL == (fp = popen(str, wr))){
+            cout << "error" << endl;
+            return;
+        }
 
-    if(NULL == (fp = popen(cmd.c_str(), "r"))){
-        cout << "error" << endl;
-        return;
+        if(NULL == in){
+            while(fgets(line, sizeof(line) - 1, fp) != NULL)
+                cout << "line : " << line ;
+        }else{
+            if(NULL != in){
+                cout << "input ..." << endl;
+                int ret = fputs(in, fp);
+                cout << "ret = " << ret << endl;
+            }
+        }
+    };
+
+    {
+        do_shell("ls -l ~/share/temp");
     }
-
-    while(fgets(line, sizeof(line) - 1, fp) != NULL){
-        cout << "line : " << line ;
+    cout << "---------------------------" << endl;
+    {
+        do_shell("ls ~/share/temp");
+    }
+    cout << "---------------------------" << endl;
+    {
+        do_shell("ls ~/share/temp; ls -l ~/share/temp");
+    }
+#if 0
+    cout << "---------------------------" << endl;
+    {
+        //do_shell("cd ~/share/workspace/wt-space/wt-server/build/LINUX; ./build-wt-server.sh");
+        do_shell("cd ~/share/workspace/wt-space/wt-server/build/LINUX; ./exe.sh");
+        //do_shell("cd ~/share/workspace/wt-space/wt-gateway/build/LINUX; ./exe.sh");
+    }
+#endif
+    cout << "---------------------------" << endl;
+    {
+        do_shell("cd ~/share/temp/test; ./a.out", "minphone1 minphone2 minphone3");
     }
 
     pclose(fp);
