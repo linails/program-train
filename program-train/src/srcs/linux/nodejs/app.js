9c92eff9eb25fcf81693f18a1846cba2344328f8
minphone <linails@foxmail.com>
2016-11-25 10:44:27 +0800

    add script-embeded-program DIR

--- program-train/src/srcs/linux/nodejs/app.js
+++ program-train/src/srcs/linux/nodejs/app.js
@@ -1,11 +1,11 @@
 /*
  * Progarm Name: app.js
  * Created Time: 2016-11-18 18:01:58
- * Last modified: 2016-11-21 09:18:00
+ * Last modified: 2016-11-23 09:18:56
  * @author: minphone.linails linails@foxmail.com 
  */
 
 function app()
 {
-    consols.log("app ...");
+    console.log("app ...");
 }
