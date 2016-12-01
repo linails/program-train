eaee1f5de323ff919b26a5d1f5b201b876b0ef6d
minphone <linails@foxmail.com>
2016-12-01 21:44:40 +0800

    update script - js

--- program-train/src/srcs/primer/exception_test.cpp
+++ program-train/src/srcs/primer/exception_test.cpp
@@ -1,7 +1,7 @@
 /*
  * Progarm Name: exception_test.cpp
  * Created Time: 2016-08-07 21:27:27
- * Last modified: 2016-08-07 22:07:56
+ * Last modified: 2016-12-01 07:14:16
  * @author: minphone.linails linails@foxmail.com 
  */
 
@@ -47,4 +47,14 @@ void exception_test(void)
         }
     }
 }
+
+/* 
+ * 析构函数在栈展开的过程中执行，这一事实影响着我们编写析构函数的方式
+ *
+ * 析构函数得能够在本函数内处理异常，不能抛出异常
+ *
+ * 在实际的编程过程中，因为析构函数仅仅是释放资源，所以它不太可能抛出异常，所有标准库
+ * 类型都能够确保它们的析构函数不会引发异常
+ * */
+
 
