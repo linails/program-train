c97573a33fa88983978e42e5dd47a5ccedbeecb7
minphone <linails@foxmail.com>
2016-12-06 23:06:03 +0800

    multiprocess & zombie / destroy zombie

--- program-train/src/srcs/linux/process/process.hpp
+++ program-train/src/srcs/linux/process/process.hpp
@@ -1,7 +1,7 @@
 /*
  * Progarm Name: process.hpp
  * Created Time: 2016-12-02 17:31:56
- * Last modified: 2016-12-02 17:34:14
+ * Last modified: 2016-12-06 22:44:30
  * @author: minphone.linails linails@foxmail.com 
  */
 
@@ -14,6 +14,20 @@
  * 而操作系统不会销毁子进程，直到把这些值传递给产生该子进程的父进程，处于这种
  * 状态下的进程就是僵尸进程
  * */
+
+class Process{
+public:
+    Process();
+    ~Process();
+    int process_main(int argc, char **argv);
+private:
+    int fork_child_porc(void);
+    int zombie(void);
+    int destroy_zombie(void);
+    int destroy_zombie(int);
+private:
+    int m_global;
+};
 
 #endif //_PROCESS_HPP_
 
