89238cf2b392bdc837f2a9b8b42ff9d5504e8840
minphone <linails@foxmail.com>
2016-12-13 11:11:51 +0800

    update d3-geo ...

--- program-train/src/srcs/linux/nodejs/app.js
+++ program-train/src/srcs/linux/nodejs/app.js
@@ -1,15 +1,42 @@
 /*
  * Progarm Name: app.js
  * Created Time: 2016-11-18 18:01:58
- * Last modified: 2016-11-30 11:22:34
+ * Last modified: 2016-12-09 14:49:03
  * @author: minphone.linails linails@foxmail.com 
  */
 
-function app()
+function printf(msg)
 {
-    console.log("app ...");
+    console.log(msg + "\n");
 }
 
-class App {
-};
+function draw(data)
+{
+    printf("data = " + data);
+}
+
+d = [1, 2, 3, 4, 5, 6, 7, 8];
+
+draw(d);
+
+d[0] = 10
+
+draw(d);
+
+function callback()
+{
+    printf("I'm callback ...");
+}
+
+function do_cb(cb)
+{
+    printf("I'm do_cb ...");
+    cb();
+}
+
+do_cb(callback);
+
+
+
+
 
