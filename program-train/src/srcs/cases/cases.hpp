8512551f5be8bd7a34d718275805d94350f5fe68
minphone <linails@foxmail.com>
2017-09-27 10:32:26 +0800

    update cases/exlib-cases/mjson.cpp ...

--- program-train/src/srcs/cases/cases.hpp
+++ program-train/src/srcs/cases/cases.hpp
@@ -1,7 +1,6 @@
 /*
  * Progarm Name: cases.hpp
  * Created Time: 2016-12-15 22:15:13
- * Last modified: 2017-08-09 19:18:30
  * @author: minphone.linails linails@foxmail.com 
  */
 
@@ -11,13 +10,13 @@
 #include "manager.hpp"
 #include "cfg-loader.hpp"
 
-#define COMPILE_FLAG_exlibCases    0
+#define COMPILE_FLAG_exlibCases    1
 #define COMPILE_FLAG_case001       0
 #define COMPILE_FLAG_case002       0
 #define COMPILE_FLAG_case003       0
 #define COMPILE_FLAG_case004       0
 #define COMPILE_FLAG_case005       0    // TS-parser
-#define COMPILE_FLAG_case006       1    // code-formatter
+#define COMPILE_FLAG_case006       0    // code-formatter
 #define COMPILE_FLAG_case_other    0
 
 class Cases{
